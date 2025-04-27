bf7173f14bdae95903b47133d2a54287a30036be
unknown <ahmedtamerasem@gmail.com>
2025-04-27 21:50:21 +0300

    first commit

---
+++ mainhome.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function clothes(cloth){
+  img.src = cloth;
+}
+function colors(color){
+ container.style.background = color;
+}
